a8ce8d795240cb16975ece1b225388a82c32ded4
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-07-01 07:11:02 -0500

    more work with the pdf. It still looks nasty in Chrome.

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,31 @@
-const run = () => {
-  const résuméModal = document.querySelector('#résumé-modal');
-  résuméModal.display = 'block';
+const body = document.querySelector('body');
+const résuméModal = document.querySelector('#résumé-modal');
+const screenDimmer = document.querySelector('#screen-dimmer');
+
+const toggleRésumé = () => {
+  résuméModal.classList.toggle('hidden');
+  if (résuméModal.className !== 'hidden') {
+    const halfWidth = résuméModal.offsetWidth / 2;
+    résuméModal.style.marginLeft = `calc(50% - ${halfWidth}px)`;
+    toggleScreenDimmer();
+  }
 }
+
+const toggleBodyScroll = () => {
+  body.classList.toggle('no-scroll');
+}
+
+const toggleScreenDimmer = () => {
+  screenDimmer.classList.toggle('hidden');
+  // toggleBodyScroll();
+}
+
+const screenDimmerHandler = () => {
+  if (résuméModal.className !== 'hidden') {
+    toggleRésumé();
+  } else {
+    toggleScreenDimmer();
+  }
+}
+
+screenDimmer.addEventListener('click', screenDimmerHandler);

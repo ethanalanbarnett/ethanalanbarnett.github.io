cfe466a20cda78453cf8b794bfe1ec491058ce07
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-07-10 19:12:05 -0500

    Rewrote the site to be SPA for the most part. I completely rewrote the JS to be OOP. The from of the site is still in the index.html page, but the content of #page is dynamic based on the 'page' being viewed. It's still super rough. I need to fix a lot of the JS to make navigation work (URI updates), and I need to keep better track of my objects. I might borrow from my big thicket app. I also rearranged the resource files to all be in one folder with subfolders for types. I think I broke my links in CSS. I might commit those right after this.

---
+++ app.js
@@ -0,0 +1,156 @@
+class App {
+  static body = document.querySelector('body');
+  static pageElement = document.querySelector('#page');
+  static currentPage;
+  static activeModal = false;
+  static inactiveModals = [];
+  static modals = {};
+  static screenDimmer;
+  static init() {
+    HomePage.render();
+    this.screenDimmer = new ScreenDimmer();
+    this.screenDimmer.render();
+    this.modals.résumé = new RésuméModal();
+    window.addEventListener('resize', Tools.resizeHandler);
+    Tools.resizeHandler();
+  }
+}
+
+class Tools {
+  static resizeHandler() {
+    Tools.responsiveHebrewTextAlignment();
+    if (App.activeModal) {
+      App.activeModal.resize();
+    }
+  }
+  static responsiveHebrewTextAlignment() {
+    if (window.innerWidth <= 1042) {
+      const aleichem = document.querySelector('#narrow-aleichem');
+      const shalom = document.querySelector('#narrow-shalom');
+      const aleichemHalfWidth = aleichem.offsetWidth / 2;
+      const shalomHalfWidth = shalom.offsetWidth / 2;
+      aleichem.style.left = `calc(18% - ${aleichemHalfWidth}px)`;
+      shalom.style.right = `calc(18% - ${shalomHalfWidth}px)`;
+    }
+  }
+}
+
+class Page {
+  static render() {
+    if (this.page !== App.currentPage) {
+      App.pageElement.innerHTML = this.content;
+      App.currentPage = this.page;
+    }
+  }
+}
+
+class HomePage extends Page {
+  static page = 'home';
+  static content = `
+    <div id="shalom-for-wide-background">
+      <h1 id="wide-shalom">שָׁלוֹם עוֹלָם</h1>
+      <!-- <p class="subscript">Shalom!</p> -->
+    </div>
+    <h2>My name is Ethan Alan Barnett</h2>
+    <table class="page-bi-table">
+      <tr>
+        <td class="bi-table-cell title-cell"><h3>Feel free to availe yourself of this cheeky "hello world!" reference:</h3></td>
+        <td class="bi-table-cell title-cell"><h3>Alternatively, you may also take a gander at my résumé (click to enlarge):</h3></td>
+      </tr>
+      <tr>
+        <td class="bi-table-cell">
+          <iframe id="youtube" src="https://www.youtube-nocookie.com/embed/kqdBD6MMciA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </td>
+        <td class="bi-table-cell">
+          <a href="javascript:App.modals.résumé.toggle()"><img id="résumé-thumbnail" src="resources/images/Ethan's Résumé.svg" alt="Ethan's Résumé"></a>
+        </td>
+      </tr>
+    </table>
+    <p>this is just some text.</p>
+  `;
+}
+
+class ArchivePage extends Page {
+  static page = 'archive';
+  static content = `
+    <h1>Archive</h1>
+  `;
+}
+
+class PortfolioPage extends Page {
+  static page = 'portfolio';
+  static content = `
+    <h1>Portfolio</h1>
+  `;
+}
+
+class Modal {
+  toggle() {    /* add functionality to remove active modal. Also, shift inactive modals to be active */
+    if (App.activeModal.modalName === this.modalName) {
+      this.element.innerHTML = '';
+    } else if (App.inactiveModals.length > 0) {
+      for (const modal of inactiveModals) {
+        if (modal.modalName === this.modalName) {
+          this.element.innerHTML = '';
+        }
+      }
+    } else {
+      this.render();
+    }
+  }
+  render() {
+    this.element = document.createElement('div');
+    this.element.id = this.modalName;
+    this.element.className = 'modal';
+    App.body.append(this.element);
+    this.element.innerHTML = this.content;
+    App.activeModal = this;
+    this.resize();
+    App.screenDimmer.toggle();
+    // this.element.addEventListener('click', this.clickHandler.bind(this));
+  }
+  resize() {
+    const halfWidth = this.element.offsetWidth / 2;
+    this.element.style.marginLeft = `calc(50% - ${halfWidth}px)`;
+  }
+}
+
+class RésuméModal extends Modal {
+  modalName = 'résumé-modal';
+  element;
+  content = `
+    <div class="modal-toolbar">
+      <table>
+        <tr>
+          <td class="modal-toolbar-btn"><a class="nav-link" href="resources/documents/Ethan's Résumé.pdf" target="_blank">Open PDF</a></td>
+          <td class="modal-toolbar-xbtn"><a class="nav-link" href="javascript:App.screenDimmer.clickHandler()" download>🗙</a></td>
+        </tr>
+      </table>
+    </div>
+    <img id="résumé" src="resources/images/Ethan's Résumé.svg" alt="Ethan's Résumé">
+  `;
+}
+
+class ScreenDimmer {
+  element;
+  render() {
+    this.element = document.createElement('div');
+    this.element.id = 'screen-dimmer';
+    this.element.className = 'hidden';
+    App.body.append(this.element);
+    this.element.addEventListener('click', this.clickHandler.bind(this));
+  }
+  toggle() {
+    this.element.classList.toggle('hidden');
+  }
+  clickHandler() {
+    if (App.activeModal) {
+      App.activeModal.toggle();
+      this.toggle()
+    } else {
+      this.toggle();
+    }
+  }
+}
+
+App.init();
---
+++ temp/appOLD.js
@@ -0,0 +1,57 @@
+const body = document.querySelector('body');
+const portfolioDropdown = document.querySelector('#portfolio-dropdown');
+const résuméModal = document.querySelector('#résumé-modal');
+const screenDimmer = document.querySelector('#screen-dimmer');
+
+// const toggleRésumé = () => {
+//   résuméModal.classList.toggle('hidden');
+//   if (résuméModal.className !== 'hidden') {
+//     resizeModal();
+//     toggleScreenDimmer();
+//   }
+// }
+
+const toggleBodyScroll = () => {
+  body.classList.toggle('no-scroll');
+}
+
+// const toggleScreenDimmer = () => {
+//   screenDimmer.classList.toggle('hidden');
+//   // toggleBodyScroll();
+// }
+
+const navDropdownHandler = () => {
+  portfolioDropdown.classList.toggle('hidden');
+}
+
+// const screenDimmerHandler = () => {
+//   if (résuméModal.className !== 'hidden') {
+//     toggleRésumé();
+//   } else {
+//     toggleScreenDimmer();
+//   }
+// }
+
+// const resizeModal = () => {
+//   const halfWidth = résuméModal.offsetWidth / 2;
+//   résuméModal.style.marginLeft = `calc(50% - ${halfWidth}px)`;  
+// }
+
+// const responsiveHebrewTextAlignment = () => {
+//   if (window.innerWidth <= 1042) {
+//     const aleichem = document.querySelector('#narrow-aleichem');
+//     const shalom = document.querySelector('#narrow-shalom');
+//     const aleichemHalfWidth = aleichem.offsetWidth / 2;
+//     const shalomHalfWidth = shalom.offsetWidth / 2;
+//     aleichem.style.left = `calc(18% - ${aleichemHalfWidth}px)`;
+//     shalom.style.right = `calc(18% - ${shalomHalfWidth}px)`;
+//   }
+// }
+
+// screenDimmer.addEventListener('click', screenDimmerHandler);
+// window.addEventListener('resize', resizeModal);
+// window.addEventListener('resize', responsiveHebrewTextAlignment);
+
+// if (window.innerWidth <= 1042) {
+//   responsiveHebrewTextAlignment();
+// }

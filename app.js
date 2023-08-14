30eb97fb5747277f303a5fab8cabb4db9d0e3038
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-08-14 11:24:38 -0500

    Reworked a lot of the uri related methods in the Tools class. It should work now and it is mostly optimized.

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ class App {
   static modals = {};
   static screenDimmer;
   static init() {
-    Tools.initUrlRoute();
     this.pages.homePage = new HomePage();
     this.pages.portfolioPage = new PortfolioPage();
     this.pages.archivePage = new ArchivePage();
+    Tools.initUriRoute();
     this.modals.résumé = new RésuméModal();
     this.screenDimmer = new ScreenDimmer();
     this.screenDimmer.render();
@@ -25,41 +25,44 @@ class App {
 }
 
 class Tools {
-  static initUrlRoute() {
+  static initUriRoute() {
     if (window.location.hash.length > 1) {
-      let path = window.location.hash.slice(2);
-      if (path[path.length - 1] === '/') {
-        path = path.slice(0, -1);
+      let uri = window.location.hash.slice(2);
+      if (uri[uri.length - 1] === '/') {
+        uri = uri.slice(0, -1);
       }
-      if (path === 'resume') {
-        window.location.href = `https://ethanalanbarnett.github.io/resources/documents/Ethan's Résumé.pdf`;
-      } else if (path === 'portfolio') {
-        history.pushState(null, '', path);
-        App.pages.portfolioPage.render('init');
-      } else if (path === 'archive') {
-        history.pushState(null, '', path);
-        App.pages.archivePage.render('init');
-      } else {
-        history.pushState(null, '', '/');
-        App.pages.homePage.render('init');
-      }
+      Tools.pageSwitch(uri);
     } else {
-      App.pages.homePage.render('init');
+      App.pages.homePage.activate('no-uri-update');
     }
   }
   static popState() {
     const uri = window.location.pathname.slice(1);
+    Tools.pageSwitch(uri, 'no-uri-update');
+  }
+  static uriUpdate(page) {
+    if (page === 'home') {
+      history.pushState(null, '', '/');
+    } else {
+      history.pushState(null, '', page);
+    }
+  }
+  static pageSwitch(uri, option) {
     switch (uri) {
-      case 'portfolio':
-        App.pages.portfolioPage.render('init');
+      case 'resume':
+        window.location.href = `https://ethanalanbarnett.github.io/resources/documents/Ethan's Résumé.pdf`;
         break;
-      case 'archive':
-        App.pages.archivePage.render('init');
+      case 'portfolio' || 'Portfolio' || 'PORTFOLIO':
+        App.pages.portfolioPage.activate(option);
         break;
-      case 'home':
-      case '':
-        App.pages.homePage.render('init');
+      case 'archive' || 'Archive' || 'ARCHIVE':
+        App.pages.archivePage.activate(option);
         break;
+      case 'home' || 'Home' || 'HOME' || '':
+        App.pages.homePage.activate(option);
+      default:
+        App.pages.notFoundPage = new NotFoundPage(uri);
+        App.pages.notFoundPage.activate(option);
     }
   }
   static resizeHandler() {
@@ -96,35 +99,42 @@ class Tools {
 
 class Page {
   activate(option) {
-    if (App.activePage.pageName !== this.pageName) {
-      if (App.inactivePages.length > 0) {
-        let i = 0;
-        for (const page of App.inactivePages) {
-          if (page.pageName === this.pageName) {
-            App.activePage.element.classList.toggle('hidden');
-            page.element.classList.toggle('hidden');
-            App.inactivePages.push(App.activePage);
-            App.activePage = page;
-            App.inactivePages.splice(i, 1);
+    if (!App.activePage) {
+      this.render(option);
+    } else if (App.activePage.pageName !== this.pageName) {
+      App.activePage.element.classList.toggle('hidden');
+      App.inactivePages.push(App.activePage);
+      let i = 0;
+      let found;
+      for (const page of App.inactivePages) {
+        if (page.pageName === this.pageName) {
+          if (option !== 'no-uri-update') {
+            Tools.uriUpdate(this.pageName);
           }
+          found = true;
+          page.element.classList.toggle('hidden');
+          App.activePage = page;
+          App.inactivePages.splice(i, 1);
+          Tools.resizeHandler();
+          break;
+        } else {
+          found = false;
+          i++
         }
-      } else {
+      }
+      if (!found) {
         this.render(option);
       }
     }
   }
   render(option) {
-    if (option !== 'init') {
-      if (this.pageName === 'home') {
-        history.pushState(null, '', '/');
-      } else {
-        history.pushState(null, '', this.pageName);
-      }
+    if (option !== 'no-uri-update') {
+      Tools.uriUpdate(this.pageName);
     }
-    App.pageElement.append(this.content);
-    if (App.activePage) {
-      App.inactivePages.push(App.activePage);
-    }
+    this.element = document.createElement('article');
+    this.element.id = `${this.pageName}-page`;
+    this.element.innerHTML = this.content;
+    App.pageElement.append(this.element);
     App.activePage = this;
     Tools.resizeHandler();
   }
@@ -134,23 +144,21 @@ class HomePage extends Page {
   pageName = 'home';
   element;
   content = `
-    <article id="home-page">
-      <h1 class="desktop-hebrew-txt centered">שָׁלוֹם עוֹלָם</h1>
-      <h2 class="main__title centered">My name is Ethan Alan Barnett</h2>
-      <div class="greeter">
-        <div class="greeter__itm">
-          <h3 class="greeter__txt">Feel free to availe yourself of this cheeky "hello world!" reference:</h3>
-          <iframe class="greeter__vid" src="https://www.youtube-nocookie.com/embed/kqdBD6MMciA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-        </div>
-        <div class="greeter__itm">
-          <h3 class="greeter__txt">Alternatively, you may also take a gander at my résumé (click to enlarge):</h3>
-          <a href="javascript:App.modals.résumé.toggle()"><img class="greeter__résumé" src="resources/images/Ethan's Résumé.svg" alt="Ethan's Résumé"></a>
-        </div>
+    <h1 class="desktop-hebrew-txt centered">שָׁלוֹם עוֹלָם</h1>
+    <h2 class="main__title centered">My name is Ethan Alan Barnett</h2>
+    <div class="greeter">
+      <div class="greeter__itm">
+        <h3 class="greeter__txt">Feel free to availe yourself of this cheeky "hello world!" reference:</h3>
+        <iframe class="greeter__vid" src="https://www.youtube-nocookie.com/embed/kqdBD6MMciA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-      <br><br>
-      <p class="body-txt">This site is obviously rough and unfinished (beware the 'Portfolio' page). Its purpose is to display my abilities as I learn front-end web development.
-      <br>At the moment, this site is running on vanilla HTML, CSS, and JS: as that is my current focus.</p>
-    </article>
+      <div class="greeter__itm">
+        <h3 class="greeter__txt">Alternatively, you may also take a gander at my résumé (click to enlarge):</h3>
+        <a href="javascript:App.modals.résumé.toggle()"><img class="greeter__résumé" src="resources/images/Ethan's Résumé.svg" alt="Ethan's Résumé"></a>
+      </div>
+    </div>
+    <br><br>
+    <p class="body-txt">This site is obviously rough and unfinished (beware the 'Portfolio' page). Its purpose is to display my abilities as I learn front-end web development.
+    <br>At the moment, this site is running on vanilla HTML, CSS, and JS: as that is my current focus.</p>
   `;
 }
 
@@ -158,26 +166,24 @@ class PortfolioPage extends Page {
   pageName = 'portfolio';
   element;
   content = `
-    <article id="portfolio-page">
-      <h1 class="main__title centered">Portfolio</h1>
-      <section class="project">
-        <div class="project__itm">
-          <h2 class="project__h2"><a class="project__link" href="portfolio/bigthicket/app.html">Big Thicket Network</a></h2>
-          <picture>
-            <!-- <source srcset="resources/images/big_thicket_network_screenshot.jxl" type="image/jxl">
-            <source srcset="resources/images/big_thicket_network_screenshot.webp" type="image/webp"> -->
-            <a href="portfolio/bigthicket/app.html">
-              <img class="project__img" src="resources/images/big_thicket_network_screenshot.png" alt="Big Thicket Network Screenshot">
-            </a>
-          </picture>
-        </div>
-        <div class="project__itm">
-          <p class="project__p">This dummy (front-end only) web app is generated with vanilla JavaScript into an otherwise empty HTML boilerplate. It allows a user to log in and create microblog posts. It is written with an object oriented approach (as well as a bad CSS approach: for now).
-          <br>Username: Ethan
-          <br>Password: Ethan123!</p>
-        </div>
-      </section>
-    </article>
+    <h1 class="main__title centered">Portfolio</h1>
+    <section class="project">
+      <div class="project__itm">
+        <h2 class="project__h2"><a class="project__link" href="portfolio/bigthicket/app.html">Big Thicket Network</a></h2>
+        <picture>
+          <!-- <source srcset="resources/images/big_thicket_network_screenshot.jxl" type="image/jxl">
+          <source srcset="resources/images/big_thicket_network_screenshot.webp" type="image/webp"> -->
+          <a href="portfolio/bigthicket/app.html">
+            <img class="project__img" src="resources/images/big_thicket_network_screenshot.png" alt="Big Thicket Network Screenshot">
+          </a>
+        </picture>
+      </div>
+      <div class="project__itm">
+        <p class="project__p">This dummy (front-end only) web app is generated with vanilla JavaScript into an otherwise empty HTML boilerplate. It allows a user to log in and create microblog posts. It is written with an object oriented approach (as well as a bad CSS approach: for now).
+        <br>Username: Ethan
+        <br>Password: Ethan123!</p>
+      </div>
+    </section>
   `;
 }
 
@@ -185,10 +191,21 @@ class ArchivePage extends Page {
   pageName = 'archive';
   element;
   content = `
-    <article id="archive-page">
-      <h1 class="main__title centered">Archive</h1>
-    </article>
+    <h1 class="main__title centered">Archive</h1>
   `;
+}
+
+class NotFoundPage extends Page {
+  pageName = '404';
+  element;
+  content;
+  constructor(uri) {
+    super();
+    content = `
+    <h1 class="main__title centered">404 Error</h1>
+    <h3 class="centered">There is no page with the name of ${uri}</h3>
+  `;
+  }
 }
 
 class Modal {
@@ -215,12 +232,13 @@ class Modal {
     let found;
     for (const modal of App.inactiveModals) {
       if (modal.modalName === this.modalName) {
+        found = true;
         this.element.remove();
         App.inactiveModals.splice(i, 1);
-        found = true;
+        break;
       } else {
-        i++;
         found = false;
+        i++;
       }
     }
     if (!found) {

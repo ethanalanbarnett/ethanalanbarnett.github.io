6584f307b66df4454d90403bf8f6454c96501623
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-09-15 16:40:55 -0500

    Fixed JS, I had forgotten to update the new classname for clamp

--- ask_life/assets/js/script.js
+++ ask_life/assets/js/script.js
@@ -1,6 +1,6 @@
 const carouselShowMoreHandler = (btn) => {
   const p = btn.previousElementSibling;
-  p.classList.toggle('services__paragraph--clamped');
+  p.classList.toggle('services__paragraph-container--clamped');
   if (btn.innerHTML === 'Read More ›') {
     btn.innerHTML = 'Show Less ‹';
   } else {

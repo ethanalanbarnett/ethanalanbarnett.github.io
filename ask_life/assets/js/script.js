009d298a0bc4ec76d0dbf5798e28763d3de8b280
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-09-23 18:20:46 -0500

    finished main functionality of hamburger menu. I still need to make navigating to '#' not make two history changes.

--- ask_life/assets/js/script.js
+++ ask_life/assets/js/script.js
@@ -1,4 +1,52 @@
+const clickCurtain = document.querySelector('#click-curtain');
 let burgerMenuOpen = false;
+
+const hamburgerToggle = () => {
+  const button = document.querySelector('#hamburger-button');
+  const menu = button.nextElementSibling;
+  menu.classList.toggle('visible-flex');
+  menu.classList.toggle('panel');
+  menu.classList.toggle('panel--positioned');
+  if (burgerMenuOpen) {
+    burgerMenuOpen = false;
+  } else {
+    burgerMenuOpen = true;
+  }
+  clickCurtain.classList.toggle('hidden');
+}
+
+const hamburgerCheck = () => {
+  if (burgerMenuOpen) {
+    hamburgerToggle();
+  }
+}
+
+const hashErase = () => {
+  history.pushState(null, '', window.location.href.replace(`#`, ''));
+}
+
+const hashCheck = () => {
+  let uri = window.location.hash.slice(1);
+  if (uri[uri.length - 1] === '/') {
+    uri = uri.slice(0, -1);
+  }
+  if (uri.length === 0) {
+    hashErase();
+  }
+}
+
+const hamburgerHandler = () => {
+  hamburgerToggle();
+}
+
+const clickCurtainHandler = () => {
+  hamburgerCheck();
+}
+
+const hashChangeHandler = () => {
+  hashCheck();
+  hamburgerCheck();
+}
 
 const carouselShowMoreHandler = btn => {
   const p = btn.previousElementSibling;
@@ -10,22 +58,5 @@ const carouselShowMoreHandler = btn => {
   }
 }
 
-const hamburgerHandler = btn => {
-  const button = document.querySelector('#hamburger-button');
-  const menu = button.nextElementSibling;
-  menu.classList.toggle('visible-flex');
-  menu.classList.toggle('panel');
-  menu.classList.toggle('panel--positioned');
-  if (burgerMenuOpen) {
-    burgerMenuOpen = false;
-  } else {
-    burgerMenuOpen = true;
-    // window.addEventListener('click', checkHamburgerMenu);
-  }
-}
-
-const checkHamburgerMenu = () => {
-  if (burgerMenuOpen) {
-    hamburgerHandler();
-  }
-}
+clickCurtain.addEventListener('click', clickCurtainHandler);
+window.addEventListener('hashchange', hashChangeHandler);

4290e8637140ee71fd7490866a1c651777966214
Ethan Alan Barnett <ethanalanbarnett@protonmail.com>
2023-11-15 18:02:08 -0600

    Added js for scrolling to top and spinning logo. Also added hashes for navigation.

---
+++ public/assets/js/script.js
@@ -0,0 +1,22 @@
+const logo = document.querySelector('#logo');
+
+const scrollToTop = () => {
+  const header = document.querySelector('#header_page');
+  const hash = window.location.hash;
+  if (hash) {
+    history.pushState(null, '', window.location.href.replace(`${hash}`, ''));
+  }
+  header.scrollIntoView();
+}
+
+const rotateLogo = () => {
+  console.log(logo);
+  logo.classList.toggle('logo-main--spin');
+}
+
+const logoClickHandler = () => {
+  scrollToTop();
+  rotateLogo();
+}
+
+logo.addEventListener('click', logoClickHandler);
